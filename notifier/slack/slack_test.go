fd2701e5476c84d0b9a3c1a88c962dd35b919498
Overlords <meder33kg@gmail.com>
2019-06-22 15:08:08 +0600

    add test for 'Run' function

--- notifier/slack/slack_test.go
+++ notifier/slack/slack_test.go
@@ -58,3 +58,28 @@ func TestNotifySlackChannel(t *testing.T) {
 		assert.Equal(t, tt.Error, err)
 	}
 }
+
+func TestRun(t *testing.T) {
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	testCase := []struct {
+		Responder httpmock.Responder
+		Hook      string
+		Message   string
+		Success   bool
+		Error     error
+	}{
+		{httpmock.NewStringResponder(200, "{}"), "TKBM/BKFLY1L/tL3RAwn9EYWMaMX", "Hello", true, nil},
+		{httpmock.NewStringResponder(400, "{}"), "TKBM/YWMaMX", "Hello", false, errors.New("StatusCode: 400")},
+	}
+
+	for _, tt := range testCase {
+		urls := fmt.Sprintf("%s/%s", SlackHookSite, tt.Hook)
+		httpmock.RegisterResponder("POST", urls, tt.Responder)
+		conf := Config{Hook: tt.Hook, MessageSuccess: tt.Message, Success: tt.Success}
+		err := conf.Run()
+		assert.Equal(t, tt.Error, err)
+	}
+}
