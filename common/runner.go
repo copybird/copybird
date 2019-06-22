4f52ecc245e09750694a9cc3d7f6a872a020e05f
miolini <miolini@gmail.com>
2019-06-22 01:15:51 -0700

    add runner run func

--- common/runner.go
+++ common/runner.go
@@ -10,3 +10,17 @@ type Runner struct {
 	moduleEncrypt core.Module
 	moduleOutputs []core.Module
 }
+
+func (r *Runner) Run() {
+	wg := sync.WaitGroup{}
+	// for input and outputs
+	wg.Add(1 + len(r.moduleOutputs)) 
+	// for compress
+	if moduleCompress != nil {
+		wg.Add(1) 
+	}
+	// for encryption
+	if moduleEncrypt != nil {
+		wg.Add(1) 
+	}
+}
