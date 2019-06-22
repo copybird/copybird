190b926992503029e1a99491d0ef9c14ae2cca53
miolini <miolini@gmail.com>
2019-06-22 01:22:57 -0700

    app runner

--- common/runner.go
+++ common/runner.go
@@ -9,6 +9,7 @@ type Runner struct {
 	moduleCompress core.Module
 	moduleEncrypt core.Module
 	moduleOutputs []core.Module
+	moduleNotifiers []core.Module
 }
 
 func (r *Runner) Run() {
@@ -22,5 +23,19 @@ func (r *Runner) Run() {
 	// for encryption
 	if moduleEncrypt != nil {
 		wg.Add(1) 
+	}
+	wg.Wait()
+}
+
+func (r *Runner) runModule(module core.Module, writer io.Writer, reader io.Reader, wg *sync.WaitGroup, chanError chan error) {
+	defer wg.Done()
+	err := module.InitPipe(writer, reader)
+	if err != nil {
+		chanError <- err
+		return
+	}
+	err = module.Run()
+	if err != nil {
+		chanError <- err
 	}
 }
