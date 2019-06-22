3ab55ae0cf6b398de5fc666df7450abaa1e7b1ae
miolini <miolini@gmail.com>
2019-06-22 01:37:38 -0700

    add module running to app runner

--- common/runner.go
+++ common/runner.go
@@ -8,14 +8,14 @@ type Runner struct {
 	moduleInput core.Module
 	moduleCompress core.Module
 	moduleEncrypt core.Module
-	moduleOutputs []core.Module
+	moduleOutput core.Module
 	moduleNotifiers []core.Module
 }
 
 func (r *Runner) Run() {
 	wg := sync.WaitGroup{}
-	// for input and outputs
-	wg.Add(1 + len(r.moduleOutputs)) 
+	// for input and output
+	wg.Add(2) 
 	// for compress
 	if moduleCompress != nil {
 		wg.Add(1) 
@@ -24,18 +24,40 @@ func (r *Runner) Run() {
 	if moduleEncrypt != nil {
 		wg.Add(1) 
 	}
+	chanError := make(chan error, 1000)
+	nextReader, nextWriter := io.Pipe()
+	go runModule(r.moduleInput, nextWriter, nil, &wg, chanError)
+	if r.moduleCompress != nil {
+		_nextReader, _nextWriter := io.Pipe()
+		go runModule(r.moduleCompress, _nextWriter, nextReader, &wg, chanError)
+		nextReader = _nextReader
+	}
+	if r.moduleEncrypt != nil {
+		_nextReader, _nextWriter := io.Pipe()
+		go runModule(r.moduleEncrypt, _nextWriter, nextReader, &wg, chanError)
+		nextReader = _nextReader
+	}
+	_nextReader, _nextWriter := io.Pipe()
+	go runModule(r.moduleOutput, nil, nextReader, &wg, chanError)
 	wg.Wait()
+	for {
+		err, ok := <- chanError
+		if !ok {
+			break
+		}
+		log.Printf("")
+	}
 }
 
 func (r *Runner) runModule(module core.Module, writer io.Writer, reader io.Reader, wg *sync.WaitGroup, chanError chan error) {
 	defer wg.Done()
 	err := module.InitPipe(writer, reader)
 	if err != nil {
-		chanError <- err
+		chanError <- &ModuleError{Module: module, Err: err}
 		return
 	}
 	err = module.Run()
 	if err != nil {
-		chanError <- err
+		chanError <- &ModuleError{Module: module, Err: err}
 	}
 }
