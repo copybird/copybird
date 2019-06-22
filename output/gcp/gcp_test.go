80b75fc45f99de6bd89b67e0821f2cac0046f9a3
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-06-22 13:37:55 +0600

    Fix tests for s3 and gcp

--- output/gcp/gcp_test.go
+++ output/gcp/gcp_test.go
@@ -30,12 +30,9 @@ func TestInitModule(t *testing.T) {
 
 	var gcp GCP
 
-	err := gcp.InitModule(map[string]string{"AWS_REGION": "us-east-1"})
+	err := gcp.InitModule(Config{CredentialsFilePath: ""})
 	require.Error(t, err, "Should fail to find credentials")
  
-	err = gcp.InitModule(map[string]string{"TOKEN_SOURCE": "some token source"})
-	require.Error(t, err, "token source is not supported")
-
-	err = gcp.InitModule(map[string]string{"CREDENTIALS_FILE": "creds.json"})
+	err = gcp.InitModule(Config{CredentialsFilePath: "creds.json"})
 	require.Error(t, err, "credentials file is missing")
 }
