956cfac07950bd9cfe6d95caa5686e68c1a50282
Overlords <meder33kg@gmail.com>
2019-06-23 10:08:33 +0600

    add initial logic for sending an aws sqs message

---
+++ modules/global/notifier/awssqs/awssqs.go
@@ -0,0 +1,47 @@
+package awssqs
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go/service/sqs/sqsiface"
+	"github.com/knative/pkg/cloudevents"
+)
+
+const (
+	// MODULE_NAME is name of a module
+	MODULE_NAME = "awssqs"
+)
+
+type Clients struct {
+	SQS         sqsiface.SQSAPI
+	CloudEvents *cloudevents.Client
+}
+
+func (c *Config) NotifyAWSSQS() error {
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(c.Region),
+		Credentials: credentials.NewStaticCredentials(c.AccountAccessKeyID, c.AccountSecretAccessKey, ""),
+		MaxRetries:  aws.Int(5),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	sqsClient := sqs.New(sess)
+
+	sendMessage := &sqs.SendMessageInput{
+		MessageBody:  aws.String(c.MessageBody),
+		QueueUrl:     aws.String(c.QueueUrl),
+		DelaySeconds: aws.Int64(3),
+	}
+
+	_, err = sqsClient.SendMessage(sendMessage)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
