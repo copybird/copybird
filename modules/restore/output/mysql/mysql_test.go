62596500dc610975dea9b3148657fb7325101aad
big-bo99 <omuraliev.baurzhan@gmail.com>
2019-06-23 18:12:52 +0600

    Updates path to test dump

--- modules/restore/output/mysql/mysql_test.go
+++ modules/restore/output/mysql/mysql_test.go
@@ -17,7 +17,7 @@ func TestRestoreOutputMysql_Run(t *testing.T) {
 	assert.Equal(t, err, nil)
 
 	// TODO: Need parse file, but after implement sql formatter
-	f, _ := os.Open("../../../..//samples/mysql.sql")
+	f, _ := os.Open("../../../../samples/mysql.sql")
 	rs.reader = bufio.NewReader(f)
 
 	err = rs.Run()
