5d39abf53cc593e671037b1d92b2753581a21dca
stanislavdev3 <stanislav.dev3@gmail.com>
2019-06-23 13:37:45 +0600

    #46 Add postgres output on restore.

---
+++ modules/restore/output/postgresql/postgresql_test.go
@@ -0,0 +1,29 @@
+package postgres
+
+import (
+	"bufio"
+	"gotest.tools/assert"
+	"os"
+	"testing"
+)
+
+var rs RestoreOutputPostgresql
+var conf = Config{
+	DSN: "host=127.0.0.1 port=5432 user=postgres password=postgres dbname=test sslmode=disable",
+}
+
+func TestRestoreOutputPostgresql_Run(t *testing.T) {
+	err := rs.InitModule(&conf)
+	assert.Equal(t, err, nil)
+
+	// TODO: Need parse file, but after implement sql formatter
+	f, _ := os.Open("./samples/postgres.sql")
+	rs.reader = bufio.NewReader(f)
+
+	//rs.reader = bytes.NewReader([]byte("connect test \n "))
+	err = rs.Run()
+	if err != nil {
+		print(err.Error())
+	}
+	assert.Equal(t, err, nil)
+}
