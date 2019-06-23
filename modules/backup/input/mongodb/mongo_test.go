a42c75960d26ea7a320a71fe6070b2c698002083
tkila <ktynybekov@gmail.com>
2019-06-23 17:31:07 +0600

    Add mongodb export

--- modules/backup/input/mongodb/mongo_test.go
+++ modules/backup/input/mongodb/mongo_test.go
@@ -1,22 +1,41 @@
 package mongodb
 
 import (
+	"context"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestMongoBackup(t *testing.T) {
+func TestMongoBackup2(t *testing.T) {
 	d := &BackupInputMongodb{}
 	c := d.GetConfig().(*MongoConfig)
 	c.DSN = "mongodb://127.0.0.1:27017"
 	require.NoError(t, d.InitModule(c))
-	names, err := d.getDatabases()
+	names, err := d.getDatabases(context.TODO())
 	assert.NoError(t, err)
 	assert.Equal(t, []string{"admin", "local", "test"}, names)
-	collections, err := d.getCollections("test")
+	collections, err := d.getCollections(context.TODO(), "test")
 	assert.NoError(t, err)
 	assert.Equal(t, []string{"link", "test"}, collections)
 
+}
+
+func TestExportCollection(t *testing.T) {
+	d := &BackupInputMongodb{}
+	c := d.GetConfig().(*MongoConfig)
+	c.DSN = "mongodb://127.0.0.1:27017"
+	require.NoError(t, d.InitModule(c))
+	tmpFile, err := os.Create("./export.txt")
+	if !assert.NoError(t, err, "tmp file") {
+		t.Fail()
+	}
+
+	d.writer = tmpFile
+
+	assert.NoError(t, d.exportCollection("admin", "system.version"), "export collection")
+	tmpFile.Close()
+	// os.Remove(tmpFile.Name())
 }
