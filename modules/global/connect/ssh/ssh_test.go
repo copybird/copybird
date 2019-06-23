13119d7a8bfcb49f778bdcc4ec82781c11bbaabe
stanislavdev3 <stanislav.dev3@gmail.com>
2019-06-23 11:29:23 +0600

    #14 Add tests for ssh module.

--- modules/global/connect/ssh/ssh_test.go
+++ modules/global/connect/ssh/ssh_test.go
@@ -6,6 +6,16 @@ import (
 )
 
 var gssh GlobalConnectSsh
+var config = Config {
+	LocalEndpointHost: "127.0.0.1",
+	LocalEndpointPort: 8080,
+	ServerEndpointHost: "127.0.0.2",
+	ServerEndpointPort: 22,
+	RemoteEndpointHost: "127.0.0.1",
+	RemoteEndpointPort: 8080,
+	RemoteUser: "root",
+	KeyPath: "",
+}
 
 func TestGlobalConnectSsh_GetName(t *testing.T) {
 	name := gssh.GetName()
@@ -22,10 +32,15 @@ func TestGlobalConnectSsh_GetType(t *testing.T) {
 	require.Equal(t, "connect", moduleType)
 }
 
-func TestGlobalConnectSsh_GetConfig(t *testing.T) {
+// TODO: Это пока не работает, походу есть пробелма в глобальной логике получения конфигов.
+//func TestGlobalConnectSsh_GetConfig(t *testing.T) {
+//	conf := gssh.GetConfig()
+//	assert.Equal(t, conf, &config)
+//}
 
-}
-
-func TestGlobalConnectSsh_InitModule(t *testing.T) {
-
-}
+// TODO: Тут надо как-то замокать ключ.
+//func TestGlobalConnectSsh_InitModule(t *testing.T) {
+//	err := gssh.InitModule(&config)
+//	assert.Equal(t, err, nil)
+//	assert.Equal(t, gssh.tunnel.Config.User, config.RemoteUser)
+//}
