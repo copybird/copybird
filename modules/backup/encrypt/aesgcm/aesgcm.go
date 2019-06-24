abc62cb77749b865e9cedbb917cc146091430e72
miolini <miolini@gmail.com>
2019-06-23 18:56:10 -0700

    fix security of aes-gcm encrypt module

--- modules/backup/encrypt/aesgcm/aesgcm.go
+++ modules/backup/encrypt/aesgcm/aesgcm.go
@@ -1,9 +1,11 @@
 package aesgcm
 
 import (
+	"bufio"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"encoding/binary"
 	"encoding/hex"
 	"errors"
 	"fmt"
@@ -15,13 +17,14 @@ import (
 const GROUP_NAME = "backup"
 const TYPE_NAME = "encrypt"
 const MODULE_NAME = "aesgcm"
+const BUF_SIZE = 4096
 
 type BackupEncryptAesgcm struct {
 	core.Module
-	reader io.Reader
-	writer io.Writer
-	gcm    cipher.AEAD
-	nonce  []byte
+	reader    io.Reader
+	writer    io.Writer
+	gcm       cipher.AEAD
+	bufReader *bufio.Reader
 }
 
 func (m *BackupEncryptAesgcm) GetGroup() core.ModuleGroup {
@@ -43,6 +46,7 @@ func (m *BackupEncryptAesgcm) GetConfig() interface{} {
 func (m *BackupEncryptAesgcm) InitPipe(w io.Writer, r io.Reader) error {
 	m.reader = r
 	m.writer = w
+	m.bufReader = bufio.NewReaderSize(m.reader, 4096)
 	return nil
 }
 
@@ -60,10 +64,7 @@ func (m *BackupEncryptAesgcm) InitModule(_cfg interface{}) error {
 	if err != nil {
 		return fmt.Errorf("cipher init err: %s", err)
 	}
-	m.nonce = make([]byte, 12)
-	if _, err = io.ReadFull(rand.Reader, m.nonce); err != nil {
-		return fmt.Errorf("nonce generate err: %s", err)
-	}
+
 	m.gcm, err = cipher.NewGCM(block)
 	if err != nil {
 		return fmt.Errorf("aes gcm init err: %s", err)
@@ -73,17 +74,36 @@ func (m *BackupEncryptAesgcm) InitModule(_cfg interface{}) error {
 
 func (m *BackupEncryptAesgcm) Run() error {
 	var err error
-	buf := make([]byte, 12)
+	var n int
+
+	nonce := make([]byte, 12)
+
+	originaData := make([]byte, BUF_SIZE)
+	encryptedData := make([]byte, BUF_SIZE +  m.gcm.Overhead())
+
 	for {
-		_, err = m.reader.Read(buf)
+		n, err = m.reader.Read(originaData)
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
 			return fmt.Errorf("read err: %s", err)
 		}
-		ciphertext := m.gcm.Seal(nil, m.nonce, buf, nil)
-		_, err = m.writer.Write(ciphertext)
+
+		if err = binary.Write(m.writer, binary.LittleEndian, int32(n)); err != nil {
+			return err
+		}
+
+		if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+			return fmt.Errorf("nonce generate err: %s", err)
+		}
+
+		if _, err = m.writer.Write(nonce); err != nil {
+			return fmt.Errorf("nonce write err: %s", err)
+		}
+
+		m.gcm.Seal(encryptedData, nonce, encryptedData, nil)
+		_, err = m.writer.Write(encryptedData)
 		if err != nil {
 			return fmt.Errorf("write err: %s", err)
 		}
